1d9caa164ae8efea8316489ed2d50a07cf9e1ae0
hacknus <linus.stoeckli@unibe.ch>
2025-11-02 10:28:36 +0100

    fix

--- esphome/components/radiator_ctrl/__init__.py
+++ esphome/components/radiator_ctrl/__init__.py
@@ -1,18 +1,49 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome import core
+from esphome.components import sensor
+from esphome.const import CONF_ID
 
-AUTO_LOAD = []
+DEPENDENCIES = ['uart', 'i2c']
 
-CONF_RADIATOR_CTRL = "radiator_ctrl"
+radiator_ctrl_ns = cg.esphome_ns.namespace('radiator_ctrl')
+RadiatorCtrl = radiator_ctrl_ns.class_('RadiatorCtrl', cg.Component)
 
-radiator_ctrl_ns = cg.esphome_ns.namespace("radiator_ctrl")
-RadiatorCtrl = radiator_ctrl_ns.class_("RadiatorCtrl", cg.Component)
+CONF_STEPPER_STEP_PIN = "step_pin"
+CONF_STEPPER_DIR_PIN = "dir_pin"
+CONF_STEPPER_ENABLE_PIN = "enable_pin"
+CONF_TMC2209_TX_PIN = "tmc2209_tx_pin"
+CONF_TMC2209_RX_PIN = "tmc2209_rx_pin"
+CONF_DISPLAY_MOSI_PIN = "display_mosi_pin"
+CONF_DISPLAY_CLK_PIN = "display_clk_pin"
+CONF_DISPLAY_CS_PIN = "display_cs_pin"
+CONF_DISPLAY_DC_PIN = "display_dc_pin"
+CONF_DISPLAY_RST_PIN = "display_rst_pin"
 
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(RadiatorCtrl),
-})
+    cv.Optional(CONF_STEPPER_STEP_PIN, default=18): cv.int_,
+    cv.Optional(CONF_STEPPER_DIR_PIN, default=19): cv.int_,
+    cv.Optional(CONF_STEPPER_ENABLE_PIN, default=21): cv.int_,
+    cv.Optional(CONF_TMC2209_TX_PIN, default=20): cv.int_,
+    cv.Optional(CONF_TMC2209_RX_PIN, default=21): cv.int_,
+    cv.Optional(CONF_DISPLAY_MOSI_PIN, default=23): cv.int_,
+    cv.Optional(CONF_DISPLAY_CLK_PIN, default=18): cv.int_,
+    cv.Optional(CONF_DISPLAY_CS_PIN, default=5): cv.int_,
+    cv.Optional(CONF_DISPLAY_DC_PIN, default=2): cv.int_,
+    cv.Optional(CONF_DISPLAY_RST_PIN, default=4): cv.int_,
+}).extend(cv.COMPONENT_SCHEMA)
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
+
+    cg.add(var.set_step_pin(config[CONF_STEPPER_STEP_PIN]))
+    cg.add(var.set_dir_pin(config[CONF_STEPPER_DIR_PIN]))
+    cg.add(var.set_enable_pin(config[CONF_STEPPER_ENABLE_PIN]))
+    cg.add(var.set_tmc2209_tx_pin(config[CONF_TMC2209_TX_PIN]))
+    cg.add(var.set_tmc2209_rx_pin(config[CONF_TMC2209_RX_PIN]))
+    cg.add(var.set_display_mosi_pin(config[CONF_DISPLAY_MOSI_PIN]))
+    cg.add(var.set_display_clk_pin(config[CONF_DISPLAY_CLK_PIN]))
+    cg.add(var.set_display_cs_pin(config[CONF_DISPLAY_CS_PIN]))
+    cg.add(var.set_display_dc_pin(config[CONF_DISPLAY_DC_PIN]))
+    cg.add(var.set_display_rst_pin(config[CONF_DISPLAY_RST_PIN]))
--- esphome/components/radiator_ctrl/radiator_ctrl.h
+++ esphome/components/radiator_ctrl/radiator_ctrl.h
@@ -5,10 +5,16 @@
 #ifndef RADIATOR_CTRL_H
 #define RADIATOR_CTRL_H
 
-#include "esphome/core/component.h"
-
 namespace esphome {
     namespace radiator_ctrl {
+
+        // Mock base class for local development
+        class Component {
+        public:
+            virtual void setup() {}
+            virtual void loop() {}
+            virtual void dump_config() {}
+        };
 
         class RadiatorCtrl : public Component {
         public:
@@ -16,8 +22,29 @@ namespace esphome {
             void loop() override;
             void dump_config() override;
 
+            void set_target_temperature(float temp) { target_temp_ = temp; }
+            float get_current_temperature() const { return current_temp_ = temp; }
+
+            // Pin setter methods for ESPHome integration
+            void set_step_pin(int pin) { step_pin_ = pin; }
+            void set_dir_pin(int pin) { dir_pin_ = pin; }
+            void set_enable_pin(int pin) { enable_pin_ = pin; }
+            void set_tmc2209_tx_pin(int pin) { tmc2209_tx_pin_ = pin; }
+            void set_tmc2209_rx_pin(int pin) { tmc2209_rx_pin_ = pin; }
+            void set_display_mosi_pin(int pin) { display_mosi_pin_ = pin; }
+            void set_display_clk_pin(int pin) { display_clk_pin_ = pin; }
+            void set_display_cs_pin(int pin) { display_cs_pin_ = pin; }
+            void set_display_dc_pin(int pin) { display_dc_pin_ = pin; }
+            void set_display_rst_pin(int pin) { display_rst_pin_ = pin; }
+
         private:
+            int step_pin_ = 18, dir_pin_ = 19, enable_pin_ = 21;
+            int tmc2209_tx_pin_ = 20, tmc2209_rx_pin_ = 21;
+            int display_mosi_pin_ = 23, display_clk_pin_ = 18, display_cs_pin_ = 5;
+            int display_dc_pin_ = 2, display_rst_pin_ = 4;
             bool initialized_ = false;
+            float target_temp_ = 20.0f;
+            float current_temp_ = 0.0f;
         };
 
     } // namespace radiator_ctrl
