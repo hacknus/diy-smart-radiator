57e6f221fab39cf2ac09291220cf56e9a895d301
hacknus <linus.stoeckli@unibe.ch>
2025-11-02 10:32:19 +0100

    gpio scheme

--- esphome/components/radiator_ctrl/__init__.py
+++ esphome/components/radiator_ctrl/__init__.py
@@ -2,6 +2,7 @@ import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import sensor
 from esphome.const import CONF_ID
+from esphome import pins
 
 DEPENDENCIES = ['uart', 'i2c']
 
@@ -21,29 +22,48 @@ CONF_DISPLAY_RST_PIN = "display_rst_pin"
 
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(RadiatorCtrl),
-    cv.Optional(CONF_STEPPER_STEP_PIN, default=18): cv.int_,
-    cv.Optional(CONF_STEPPER_DIR_PIN, default=19): cv.int_,
-    cv.Optional(CONF_STEPPER_ENABLE_PIN, default=21): cv.int_,
-    cv.Optional(CONF_TMC2209_TX_PIN, default=20): cv.int_,
-    cv.Optional(CONF_TMC2209_RX_PIN, default=21): cv.int_,
-    cv.Optional(CONF_DISPLAY_MOSI_PIN, default=23): cv.int_,
-    cv.Optional(CONF_DISPLAY_CLK_PIN, default=18): cv.int_,
-    cv.Optional(CONF_DISPLAY_CS_PIN, default=5): cv.int_,
-    cv.Optional(CONF_DISPLAY_DC_PIN, default=2): cv.int_,
-    cv.Optional(CONF_DISPLAY_RST_PIN, default=4): cv.int_,
+    cv.Optional(CONF_STEPPER_STEP_PIN, default=18): pins.gpio_output_pin_schema,
+    cv.Optional(CONF_STEPPER_DIR_PIN, default=19): pins.gpio_output_pin_schema,
+    cv.Optional(CONF_STEPPER_ENABLE_PIN, default=21): pins.gpio_output_pin_schema,
+    cv.Optional(CONF_TMC2209_TX_PIN, default=20): pins.gpio_output_pin_schema,
+    cv.Optional(CONF_TMC2209_RX_PIN, default=21): pins.gpio_input_pin_schema,
+    cv.Optional(CONF_DISPLAY_MOSI_PIN, default=23): pins.gpio_output_pin_schema,
+    cv.Optional(CONF_DISPLAY_CLK_PIN, default=18): pins.gpio_output_pin_schema,
+    cv.Optional(CONF_DISPLAY_CS_PIN, default=5): pins.gpio_output_pin_schema,
+    cv.Optional(CONF_DISPLAY_DC_PIN, default=2): pins.gpio_output_pin_schema,
+    cv.Optional(CONF_DISPLAY_RST_PIN, default=4): pins.gpio_output_pin_schema,
 }).extend(cv.COMPONENT_SCHEMA)
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
 
-    cg.add(var.set_step_pin(config[CONF_STEPPER_STEP_PIN]))
-    cg.add(var.set_dir_pin(config[CONF_STEPPER_DIR_PIN]))
-    cg.add(var.set_enable_pin(config[CONF_STEPPER_ENABLE_PIN]))
-    cg.add(var.set_tmc2209_tx_pin(config[CONF_TMC2209_TX_PIN]))
-    cg.add(var.set_tmc2209_rx_pin(config[CONF_TMC2209_RX_PIN]))
-    cg.add(var.set_display_mosi_pin(config[CONF_DISPLAY_MOSI_PIN]))
-    cg.add(var.set_display_clk_pin(config[CONF_DISPLAY_CLK_PIN]))
-    cg.add(var.set_display_cs_pin(config[CONF_DISPLAY_CS_PIN]))
-    cg.add(var.set_display_dc_pin(config[CONF_DISPLAY_DC_PIN]))
-    cg.add(var.set_display_rst_pin(config[CONF_DISPLAY_RST_PIN]))
+    step_pin = await cg.gpio_pin_expression(config[CONF_STEPPER_STEP_PIN])
+    cg.add(var.set_step_pin(step_pin))
+
+    dir_pin = await cg.gpio_pin_expression(config[CONF_STEPPER_DIR_PIN])
+    cg.add(var.set_dir_pin(dir_pin))
+
+    enable_pin = await cg.gpio_pin_expression(config[CONF_STEPPER_ENABLE_PIN])
+    cg.add(var.set_enable_pin(enable_pin))
+
+    tx_pin = await cg.gpio_pin_expression(config[CONF_TMC2209_TX_PIN])
+    cg.add(var.set_tmc2209_tx_pin(tx_pin))
+
+    rx_pin = await cg.gpio_pin_expression(config[CONF_TMC2209_RX_PIN])
+    cg.add(var.set_tmc2209_rx_pin(rx_pin))
+
+    mosi_pin = await cg.gpio_pin_expression(config[CONF_DISPLAY_MOSI_PIN])
+    cg.add(var.set_display_mosi_pin(mosi_pin))
+
+    clk_pin = await cg.gpio_pin_expression(config[CONF_DISPLAY_CLK_PIN])
+    cg.add(var.set_display_clk_pin(clk_pin))
+
+    cs_pin = await cg.gpio_pin_expression(config[CONF_DISPLAY_CS_PIN])
+    cg.add(var.set_display_cs_pin(cs_pin))
+
+    dc_pin = await cg.gpio_pin_expression(config[CONF_DISPLAY_DC_PIN])
+    cg.add(var.set_display_dc_pin(dc_pin))
+
+    rst_pin = await cg.gpio_pin_expression(config[CONF_DISPLAY_RST_PIN])
+    cg.add(var.set_display_rst_pin(rst_pin))
